a5f71d2773df4d3e9faf306db9d41e270c02a6bd
Shu Zhu <shu.zhu@erstegroup.com>
2018-11-07 10:51:08 +0100

    mandelbrot: add new page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Main } from './Main.jsx';
 import { LSystem } from './lsystem/LSystem.jsx';
+import { JuliaSet } from './mandelbrot/JuliaSet.jsx';
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <Switch>
                 <Route exact path="/" component={Main} />
                 <Route path="/lsystem" component={LSystem} />
+                <Route path="/mandelbrot" component={JuliaSet} />
             </Switch>
         </main>
     );
